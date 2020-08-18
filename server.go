9385387c0d5537acdade5336916c3222718885df
cesarFuhr <cesar.cara@protonmail.com>
2020-08-18 17:26:28 -0300

    WYP httpServer testing request body

--- server.go
+++ server.go
@@ -1,13 +1,44 @@
 package main
 
 import (
-	"fmt"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/json"
+	"encoding/pem"
 	"net/http"
+	"time"
+
+	"github.com/cesarFuhr/gocrypto/keys"
 )
 
-func KeyServer(w http.ResponseWriter, r *http.Request) {
+type createKeyResponseBody struct {
+	KeyID      string    `json:"keyID"`
+	Expiration time.Time `json:"expiration"`
+	PublicKey  string    `json:"publicKey"`
+}
+
+type keyStoreInterface interface {
+	CreateKey(string, time.Time) keys.Key
+}
+
+// KeyServer key HTTP API server
+type KeyServer struct {
+	keyStore keyStoreInterface
+}
+
+// ServeHTTP serves http requests
+func (s *KeyServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	key := s.keyStore.CreateKey("test", time.Now())
+
 	w.WriteHeader(http.StatusCreated)
-	fmt.Fprintf(w, `{ 
-		"publicKey": "-----BEGIN RSA PUBLIC KEY-----\n\tMIGJAoGBAKE4I3DWqAAd8z6SsrmibIneKeHXP8gfvKuHPi7ePv3ZnK1AlfToHQ16\n\tirvEMcEoa+BNQEAbBot8aB0UrvwuhMXyWHQ9ugf7AShfMNteTbsdbvKv10W0Hee9\n\tLnU+xYxmRgUNUPwba3xJG8oIshkwYXEQbQW+ys1sbTv6ohmGXdv7AgMBAAE=\n\t-----END RSA PUBLIC KEY-----"
-		}`)
+	json.NewEncoder(w).Encode(createKeyResponseBody{
+		KeyID:      key.ID,
+		Expiration: key.Expiration.UTC(),
+		PublicKey:  formatPublicKey(key.Pub),
+	})
+}
+
+func formatPublicKey(pubKey *rsa.PublicKey) string {
+	b := pem.EncodeToMemory(&pem.Block{Type: "RSA PUBLIC KEY", Bytes: x509.MarshalPKCS1PublicKey(pubKey)})
+	return string(b)
 }
--- server_test.go
+++ server_test.go
@@ -1,19 +1,42 @@
 package main
 
 import (
+	"bytes"
+	"crypto/rsa"
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"reflect"
 	"testing"
+	"time"
+
+	"github.com/cesarFuhr/gocrypto/keys"
 )
 
+type KeyStoreStub struct {
+	CalledWith []interface{}
+}
+
+func (s *KeyStoreStub) CreateKey(scope string, exp time.Time) keys.Key {
+	s.CalledWith = []interface{}{scope, exp}
+	return keys.Key{
+		Scope:      scope,
+		Expiration: time.Now().AddDate(0, 0, 1),
+		ID:         "a key ID",
+		Pub:        &rsa.PublicKey{},
+		Priv:       &rsa.PrivateKey{},
+	}
+}
+
 func TestPOSTKeys(t *testing.T) {
+	keyStoreStub := KeyStoreStub{}
+	server := KeyServer{&keyStoreStub}
 	t.Run("Should return 200 on /keys", func(t *testing.T) {
 		request, _ := http.NewRequest(http.MethodPost, "/keys", nil)
 		response := httptest.NewRecorder()
 
-		KeyServer(response, request)
+		server.ServeHTTP(response, request)
 
 		assertStatus(t, response.Code, http.StatusCreated)
 	})
@@ -21,7 +44,7 @@ func TestPOSTKeys(t *testing.T) {
 		request, _ := http.NewRequest(http.MethodPost, "/keys", nil)
 		response := httptest.NewRecorder()
 
-		KeyServer(response, request)
+		server.ServeHTTP(response, request)
 
 		respBytes, _ := ioutil.ReadAll(response.Body)
 
@@ -29,18 +52,38 @@ func TestPOSTKeys(t *testing.T) {
 			t.Errorf("got an invalid JSON %q", respBytes)
 		}
 	})
-	t.Run("Should have the publicKey property on /keys response", func(t *testing.T) {
+	t.Run("Should have the all properties on /keys response", func(t *testing.T) {
 		request, _ := http.NewRequest(http.MethodPost, "/keys", nil)
 		response := httptest.NewRecorder()
 
-		KeyServer(response, request)
-		respBytes, _ := ioutil.ReadAll(response.Body)
-		respMap := map[string]interface{}{}
-		_ = json.Unmarshal(respBytes, &respMap)
+		wants := []string{"publicKey", "keyID", "expiration"}
 
-		if _, ok := respMap["publicKey"]; ok != true {
-			t.Errorf("does not have the publicKey prop")
+		server.ServeHTTP(response, request)
+		respMap := map[string]interface{}{}
+		extractJson(response.Body, respMap)
+
+		for _, want := range wants {
+			if _, ok := respMap[want]; ok != true {
+				t.Errorf("does not have the %q prop", want)
+			}
 		}
+	})
+	t.Run("Should call the CreateKey with expiration and scope", func(t *testing.T) {
+		scope := "test"
+		expiration := time.Now().AddDate(0, 0, 1).UTC().Format(time.RFC3339)
+		requestBody, _ := json.Marshal(map[string]string{
+			"scope": scope,
+			"expiration": expiration,
+		})
+		request, _ := http.NewRequest(http.MethodPost, "/keys", bytes.NewBuffer(requestBody))
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+		respMap := map[string]interface{}{}
+		extractJson(response.Body, respMap)
+
+		assertPresence(t, keyStoreStub.CalledWith, expiration)
+		assertPresence(t, keyStoreStub.CalledWith, scope)
 	})
 }
 
@@ -48,5 +91,31 @@ func assertStatus(t *testing.T, got, want int) {
 	t.Helper()
 	if got != want {
 		t.Errorf("got %d, want %d", got, want)
+	}
+}
+
+func extractJson(jBuff *bytes.Buffer, m map[string]interface{}) error {
+	respBytes, _ := ioutil.ReadAll(jBuff)
+	_ = json.Unmarshal(respBytes, &m)
+	return nil
+}
+
+func assertParams(t *testing.T, got, want []interface{}) {
+	t.Helper()
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
+
+func assertInsideSlice(t *testing.T, a []interface{}, want interface{}) {
+    t.Helper()
+	has := false
+	for _, v := range a {
+		if reflect.DeepEqual(v, want) {
+			has = true
+		}
+	}
+	if !has {
+		t.Errorf("Did not found: %v", want)
 	}
 }
