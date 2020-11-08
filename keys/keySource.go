ed173b733718c00ce09a435601b421a804c67841
cesarFuhr <cesar.cara@protonmail.com>
2020-11-07 22:10:07 -0300

    WYP poolKeysource

--- keys/keySource.go
+++ keys/keySource.go
@@ -3,6 +3,7 @@ package keys
 import (
 	"crypto/rand"
 	"crypto/rsa"
+	"io"
 )
 
 // SynchronousKeySource simple key source
@@ -11,4 +12,31 @@ type SynchronousKeySource struct{}
 // Take Takes one key from the source
 func (s *SynchronousKeySource) Take() (*rsa.PrivateKey, error) {
 	return rsa.GenerateKey(rand.Reader, 2048)
+}
+
+type keyGenerator interface {
+	GenerateKey(io.Reader, int) (*rsa.PrivateKey, error)
+}
+
+// PoolKeySource a key source based on a pool of keys
+type PoolKeySource struct {
+	pool chan *rsa.PrivateKey
+	kgen keyGenerator
+}
+
+// Take Takes one key from the source
+func (s *PoolKeySource) Take() (*rsa.PrivateKey, error) {
+	go s.addKeyToPoll()
+	if len(s.pool) > 0 {
+		k := <-s.pool
+		return k, nil
+	}
+	return s.kgen.GenerateKey(rand.Reader, 2048)
+}
+
+func (s *PoolKeySource) addKeyToPoll() {
+	if len(s.pool) < cap(s.pool) {
+		k, _ := s.kgen.GenerateKey(rand.Reader, 2048)
+		s.pool <- k
+	}
 }
--- keys/keySource_test.go
+++ keys/keySource_test.go
@@ -2,16 +2,72 @@ package keys
 
 import (
 	"crypto/rsa"
+	"io"
+	"sync"
 	"testing"
 )
 
-func InMemTakeTest(t *testing.T) {
-	keySource := InMemoryKeySource{}
+type keyGeneratorStub struct {
+	called int
+	stored int
+	wg     sync.WaitGroup
+	mu     sync.Mutex
+}
+
+func (g *keyGeneratorStub) GenerateKey(r io.Reader, s int) (*rsa.PrivateKey, error) {
+	g.mu.Lock()
+	g.called++
+	g.mu.Unlock()
+
+	return rsa.GenerateKey(r, s)
+}
+
+func TestSyncTake(t *testing.T) {
+	keySource := SynchronousKeySource{}
 
 	t.Run("Should return a valid rsa PrivKey", func(t *testing.T) {
 		got, _ := keySource.Take()
-		want := rsa.PrivateKey{}
+		want := mockKeys
 
 		assertType(t, got, want)
 	})
+}
+
+func TestPoolTake(t *testing.T) {
+	var waitGroup sync.WaitGroup
+	keyGenStub := keyGeneratorStub{wg: waitGroup}
+	keySource := PoolKeySource{make(chan *rsa.PrivateKey, 2), &keyGenStub}
+
+	t.Run("returns a valid rsa PrivKey", func(t *testing.T) {
+		got, _ := keySource.Take()
+		want := mockKeys
+
+		<-keySource.pool
+		assertType(t, got, want)
+	})
+	t.Run("if there is no keys in the pool calls GenerateKey, and create one ascyncronouslly", func(t *testing.T) {
+		keyGenStub.called = 0
+		got, _ := keySource.Take()
+		want := mockKeys
+
+		<-keySource.pool
+		assertType(t, got, want)
+	})
+	t.Run("if there is keys in the pool should not call GenerateKey, pop one key and create one ascyncronouslly", func(t *testing.T) {
+		keySource.pool <- mockKeys
+		keyGenStub.called = 0
+		keySource.Take()
+
+		<-keySource.pool
+		assertValue(t, keyGenStub.called, 1)
+		assertValue(t, len(keySource.pool), 0)
+	})
+}
+
+func assertValue(t *testing.T, g, w interface{}) {
+	t.Helper()
+
+	if w != g {
+		t.Errorf("want %v, got %v", w, g)
+	}
 }
